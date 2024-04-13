5bd421d65283e2a21d84842cad305cc90cbd2c35
HorneOnne <anhlaquyax@gmail.com>
2024-04-13 18:16:33 +0700

    Add theme

---
+++ Assets/Game4/Scripts/UIs/ThemeDataSO.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu()]
+public class ThemeDataSO : ScriptableObject
+{
+    [Header("Menu")]
+    public Sprite MenuBtn;
+
+    [Header("Settings")]
+    public Sprite SettingsPanel;
+    public Sprite SettingsGalleryBtn;
+    public Sprite SettingsOkBtn;
+    public Sprite SoundOn;
+    public Sprite SoundOff;
+
+    [Header("Gallery")]
+    public Sprite GalleryPanel;
+    public Sprite GalleryCloseBtn;
+    public Sprite GalleryUnlockSlot;
+    public Sprite GalleryLockSlot;
+
+
+    [Header("Gameplay")]
+    public Sprite GameplayPanel;
+    public Sprite GameplayChangeThemeBtn;
+    public Sprite GameplaySettingsBtn;
+
+}
